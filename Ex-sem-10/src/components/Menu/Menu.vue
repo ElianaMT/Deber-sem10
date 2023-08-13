0c22a387b800dfd0f80cc51290176ed215e3a1b5
ElianaMT <eliana_taco@estudante.sesisenai.org.br>
2023-08-13 18:02:07 -0500

    Ex-2-Cria tela Sobre

---
+++ Ex-sem-10/src/components/Menu/Menu.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="box">
+
+        <div class="rectangle-elipse">
+            <div class="elipse" />
+            <div class="elipse" />
+            <div class="elipse" />
+        </div>
+
+    </div>
+
+    <div class="container-botones">
+           <router-link to="/">
+            <div class="btn">Home <img width="40" src="./casa.png" alt="home"></div>
+        </router-link>
+
+        <router-link to="/talentos/novo">
+            <div class="btn">Talentos <img width="40" src="./carpeta.png" alt="talent"></div>
+        </router-link>
+
+        <router-link to="/sobre">
+            <div class="btn">Sobre <img width="40" src="./pin-mapa.png" alt="ubicacion"></div>
+        </router-link>
+    </div>
+</template>
+
+<style scoped>
+.box {
+    background-color: #d9d9d9;
+    height: 76px;
+    left: 0;
+    position: fixed;
+    top: 0;
+    width: 100%;
+}
+
+.rectangle-elipse {
+    display: flex;
+    flex-direction: row;
+    padding: 23px;
+    gap: 20px;
+}
+
+.elipse {
+    background-color: #c3baba;
+    border-radius: 11px/10px;
+    box-shadow: 0px 4px 4px #00000040;
+    height: 20px;
+    width: 22px;
+}
+
+.container-botones {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    font-size: 16px;
+    font-weight: bold;
+    color: grey;
+    padding: 80px 20%;
+    justify-content: space-between;
+}
+
+.btn {
+    display: flex;
+    padding: 15px 70px;
+    border: solid gray;
+}
+</style>
---
+++ Ex-sem-10/src/views/About/About.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="texto">
+    <h1>Sobre</h1>
+    <div>
+       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem omnis eaque deserunt vel mollitia quas quaerat minima ex non consequatur doloribus doloremque, ab molestiae suscipit praesentium vero aperiam porro, libero amet error? Quisquam rerum itaque sunt quidem, accusamus eaque cupiditate adipisci voluptate illum nam, reprehenderit, ea ipsum maiores modi. Corporis vitae odit eaque consectetur? Vitae molestiae necessitatibus eligendi? A eveniet aperiam soluta esse ipsa quae accusamus omnis, eum nostrum dolorem, praesentium, laborum sint sapiente natus dolorum laboriosam aliquam. Molestiae repudiandae odit nostrum enim esse distinctio et cupiditate obcaecati repellat quis, debitis eaque dolor, fuga nesciunt quia illum dolores. Quae, earum!
+    </div>
+
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam totam fugit magni consequatur ipsa minus obcaecati aliquid libero error, perspiciatis atque at placeat cum impedit fuga magnam fugiat non? Accusantium!        
+    </div>
+
+    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos velit natus aspernatur earum maiores deleniti a dolor totam aliquid inventore, recusandae obcaecati omnis hic cum molestiae non commodi nobis exercitationem.
+    </div>
+    
+</div>
+</template>
+
+<style>
+.texto{ 
+padding: 0px 20%;
+display: flex;
+flex-direction: column;
+gap: 40px;
+}
+</style>
